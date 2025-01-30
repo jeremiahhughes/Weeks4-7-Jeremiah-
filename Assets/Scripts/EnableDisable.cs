55e9aafe434442d4aa67e6b3f2e9e43d5792e59e
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2025-01-30 10:57:21 -0500

    using Audio clips

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -8,6 +8,8 @@ public class EnableDisable : MonoBehaviour
     public SpriteRenderer sr;
     public EnableDisable script;
     public GameObject go;
+    public AudioSource audioSource;
+    public AudioClip clip;
     // Update is called once per frame
     void Update()
     {
@@ -22,6 +24,13 @@ public class EnableDisable : MonoBehaviour
             sr.enabled = true;
             go.SetActive(true);
             //script.enabled = true;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(audioSource.isPlaying == false)
+            {
+                audioSource.PlayOneShot(clip);
+            }
         }
     }
 }
