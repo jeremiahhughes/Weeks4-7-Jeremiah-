41e397f9e5a79d6b9c31a94710ca40d27d83d2dd
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2025-01-30 10:31:03 -0500

    Commit 1: Making things appear and reappear.

---
+++ Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public EnableDisable script;
+    public GameObject go;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = false;
+            go.SetActive(false);
+            //script.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            go.SetActive(true);
+            //script.enabled = true;
+        }
+    }
+}
